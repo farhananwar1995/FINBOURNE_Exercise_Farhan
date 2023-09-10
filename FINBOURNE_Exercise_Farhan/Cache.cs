7d466e8e24553c92ddb5508872c0ca8ec5f87e1a
Farhan Ghumman <farhan.beds@gmail.com>
2023-09-10 19:08:13 +0100

    Add project files.

---
+++ FINBOURNE_Exercise_Farhan/Cache.cs
@@ -0,0 +1,99 @@
+﻿
+namespace FINBOURNE_Exercise_Farhan;
+
+public class CacheItem<T>
+{
+    public DateTime DateTime { get; set; }
+    public T Item { get; set; }
+    public string Key { get; set; }
+}
+
+public class Cache<T> : IDisposable
+{
+    private readonly Dictionary<string, CacheItem<T>> _items = new Dictionary<string, CacheItem<T>>();
+    private readonly int _maxItems;
+    private readonly EvictionPolicy _evictionPolicy;
+    private readonly Dictionary<string, CacheItem<T>> _evictedItems = new Dictionary<string, CacheItem<T>>();
+
+    public Cache(int maxItems, EvictionPolicy evictionPolicy)
+    {
+        _maxItems = maxItems;
+        _evictionPolicy = evictionPolicy;
+    }
+
+    public void Add(T item, string key)
+    {
+        if (_items.Count >= _maxItems)
+        {
+            EvictItem();
+        }
+
+        var cacheItem = new CacheItem<T>();
+        cacheItem.DateTime = DateTime.Now;
+        cacheItem.Item = item;
+        cacheItem.Key = key;
+        _items[key] = cacheItem;
+    }
+
+    public bool TryGet(string key, out T value)
+    {
+        value = default(T); 
+
+        if (_items.ContainsKey(key))
+        {
+            value = _items[key].Item;
+            return true; // Key found
+        }
+        else
+        {
+            return false; // Key not found
+        }
+    }
+
+    public void Remove(string key)
+    {
+        _items.Remove(key);
+    }
+
+    public bool IsFull()
+    {
+        return _items.Count >= _maxItems;
+    }
+
+    public event EventHandler<ItemEvictedEventArgs> ItemEvicted;
+
+    protected virtual void OnItemEvicted(ItemEvictedEventArgs e)
+    {
+        ItemEvicted?.Invoke(this, e);
+    }
+
+    private void EvictItem()
+    {
+        var leastRecentlyUsedItem = _items.Values.OrderBy(x => x.DateTime).First();
+        _evictedItems[leastRecentlyUsedItem.Key] = leastRecentlyUsedItem;
+        _items.Remove(leastRecentlyUsedItem.Key);
+        OnItemEvicted(new ItemEvictedEventArgs(leastRecentlyUsedItem));
+    }
+
+    public void Dispose()
+    {
+        _items.Clear();
+        _evictedItems.Clear();
+    }
+
+    public enum EvictionPolicy
+    {
+        LeastRecentlyUsed,
+        FirstInFirstOut
+    }
+
+    public class ItemEvictedEventArgs : EventArgs
+    {
+        public readonly CacheItem<T> Item;
+
+        public ItemEvictedEventArgs(CacheItem<T> item)
+        {
+            Item = item;
+        }
+    }
+}
---
+++ FINBOURNE_Exercise_Farhan/Program.cs
@@ -0,0 +1,38 @@
+﻿using FINBOURNE_Exercise_Farhan;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var cache = new Cache<int>(10, Cache<int>.EvictionPolicy.LeastRecentlyUsed);
+
+        cache.Add(1, "One");
+        cache.Add(2, "Two");
+        cache.Add(3, "Three");
+
+        if (cache.TryGet("One", out int value))
+        {
+            Console.WriteLine("The value of key 'One' is: {0}", value);
+        }
+        else
+        {
+            Console.WriteLine("Key 'One' not found in the cache.");
+        }
+
+        Console.WriteLine("The cache is full: {0}", cache.IsFull());
+
+        cache.Remove("One");
+
+
+        if (cache.TryGet("One", out int removedValue))
+        {
+            Console.WriteLine("The value of key 'One' is: {0}", removedValue);
+        }
+        else
+        {
+            Console.WriteLine("Key 'One' not found in the cache.");
+        }
+
+        Console.ReadLine();
+    }
+}
